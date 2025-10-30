a8185c5a496841da78d71faff2598fafe075db25
Kayky Moura <kaykynascimentoep@gmail.com>
2025-10-30 16:47:52 -0300

    Feat: Subindo atualizado

--- NexusAPI/obj/Debug/net9.0/NexusAPI.AssemblyInfo.cs
+++ NexusAPI/obj/Debug/net9.0/NexusAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexusAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27aad7b976a9cf8c43383a2e6e36282847ce3d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52da6c3cb3ae68dfd0f89ef96467a4c1dd6bec83")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexusAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexusAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
